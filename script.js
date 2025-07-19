4fc14b1615ee5645ae20d189dc9e9d20897b8ddd
Ali Wangara <Aliwangara63@gmail.com>
2025-07-19 14:08:53 +0300

    added fetch function and manipulated the dom to now to display fetched content

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 const quoteApi = 'https://api.quotable.io/random'
+const quote = document.getElementById('quote')
+const author = document.getElementById('author');
+
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+async function fethApi(){
+
+    const response = await fetch(quoteApi);
+
+    const apiResponse = await response.json()
+
+    quote.innerHTML = `${apiResponse.content}
+     \n ~ ${apiResponse.author}`
+
+    console.log(apiResponse)
+
+}
+
+
+
+newQuoteBtn.addEventListener('click', ()=>{
+    fethApi()
+
+
+})
+
